4c975849b1b77c46de1b46ab298fc21990a29622
Joy Gao <joyg@wepay.com>
2015-08-22 15:14:19 -0700

    made endpoints work

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import reverse_geocoder as rg
 import us
-from flask import Flask, jsonify, make_response, request, json
+from flask import Flask, Response, jsonify, make_response, request, json
 from sklearn.externals import joblib
 from datetime import datetime
 from models import *
@@ -12,69 +12,47 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-
 @app.route('/')
 def index():
 		return "Woot!"
 
-# @app.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error):
 		return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/get_ratings', methods=['POST'])
 def get_ratings():
-		input_data = request.get_json()
-		data_for_ml = gather_data_for_ml(input_data)
-		rating = predict_rating(input_data)
-		return jsonify({'rating':rating})
+		input_data = str(request.data)
+		r = json.loads(input_data)
+		data_for_ml = gather_data_for_ml(r)
+		rating = predict_rating(data_for_ml)
+		return jsonify({'result' : rating})
+		
 
 @app.route('/get_reviews', methods=['POST'])
 def get_reviews():
-	input_data = request.get_json()
-	lat = input_data['latitude']
-	lon = input_data['longitude']
-	return 'get_reviews'
+	input_data = str(request.data)
+	r = json.loads(input_data)
+	lat = r['latitude']
+	lon = r['longitude']
+	return 'reviews!'
+
 
 @app.route('/get_nearby', methods=['POST'])
 def get_nearby():
-	input_data = request.get_json()
-	lat = input_data['latitude']
-	lon = input_data['longitude']
-	result = calculate_closest_places(lat, lon)
+	input_data = str(request.data)
+	r = json.loads(input_data)
+	result = calculate_closest_places(r['longitude'], r['latitude'])
 	return jsonify({'nearby' : result})
 
-if __name__ == '__main__':
-		app.run(debug=True)
 
 #############################################################################
 ############################## helper functions #############################
 #############################################################################
 
-def gather_data_for_ml(input_data):
-	model_ordered_list = []
-	city, state = get_city_and_state(input_data['latitude'], input_data['longitude'])
-	hours = get_hours(input_data)
-	model_ordered_list.extend([city, state]) # city, state
-	model_ordered_list.extend(hours)
-	model_ordered_list.append(input['accepts_credit_card'])
-	model_ordered_list.append(input['host_large_group'])
-	model_ordered_list.append(input['price_range'])
-	model_ordered_list.append(input['alcohol'])
-	model_ordered_list.append(input['noise_level'])
-	model_ordered_list.append(input['waiter_service'])
-	model_ordered_list.append(input['wifi'])
-	model_ordered_list.append(get_late_night_bool(input_data))
-	model_ordered_list.append(input['has_tv'])
-	model_ordered_list.append(get_24_hr_bool(hours))
-	model_ordered_list.append(input['drive_thru'])
-	model_ordered_list.extend(get_food_category_list(input_data))
-	model_ordered_list.append(calculate_closest_places(input_data['longitude'], input_data['latitude']))
-	print model_ordered_list
-	return model_ordered_list
-
 def predict_rating(dataArray):
 		clf = joblib.load(app.config['TRAINING_MODEL_FILEPATH'])
-		result = clf.predict(dataArray)
+		result = clf.predict(dataArray)[0]
 		return result
 
 def get_city_and_state(lat, lon):
@@ -146,7 +124,6 @@ def get_food_category_list(input_data):
 				results.append(1)
 			else:
 				results.append(0)
-		print results
 		return results
 
 def calculate_closest_places(lon, lat, dist=1):
@@ -163,40 +140,28 @@ def generate_tips(inputs):
 		pass
 
 
-## test datasets
-# dataArray = [63, 6, 15.0, 15.0, 24.0, 15.0, 15.0, 24.0, 24.0, True, True, 1.0, 2,
-# 						1, True, 1, False, True, False, False, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-# 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-# 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-# 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15]
+def gather_data_for_ml(input_data):
+	model_ordered_list = []
+	a = input_data['latitude']
+	b = input_data['longitude']
+	city, state = get_city_and_state(a, b)
+	hours = get_hours(input_data)
+	model_ordered_list.extend([city, state]) # city, state
+	model_ordered_list.extend(hours)
+	model_ordered_list.append(input['accepts_credit_card']) #bool
+	model_ordered_list.append(input['host_large_group']) #bool
+	model_ordered_list.append(input['price_range'])
+	model_ordered_list.append(input['alcohol'])
+	model_ordered_list.append(input['noise_level'])
+	model_ordered_list.append(input['waiter_service']) #bool
+	model_ordered_list.append(input['wifi'])
+	model_ordered_list.append(get_late_night_bool(input_data))
+	model_ordered_list.append(input['has_tv']) #bool
+	model_ordered_list.append(get_24_hr_bool(hours)) #bool
+	model_ordered_list.append(input['drive_thru']) #bool
+	model_ordered_list.extend(get_food_category_list(input_data))
+	model_ordered_list.append(calculate_closest_places(input_data['longitude'], input_data['latitude']))
+	return model_ordered_list
 
-## test input
-# input = {
-# 	'latitude': 34.23, 			
-# 	'longitude': -112.23,  			
-# 	'category' : ['Turkish', 'Taiwanese', 'Ethnic Food'],
-# 	'monday_open': '8AM',
-# 	'monday_close': '12AM',
-# 	'tuesday_open': '8AM',
-# 	'tuesday_close': '12AM',
-# 	'wednesday_open': '8AM',
-# 	'wednesday_close': '12AM',
-# 	'thurday_open': '8AM',
-# 	'thurday_close':'12AM',
-# 	'friday_open': '8AM',
-# 	'friday_close':'12AM',
-# 	'saturday_open': '8AM',
-# 	'saturday_close':'12AM',
-# 	'sunday_open' : '9AM',
-# 	'sunday_close' : '11PM',
-# 	'noise_level' : 1, 	
-# 	'wifi': 2,			
-# 	'alcohol': 2,	
-# 	'price_range' : 2, 	
-# 	'late_night': True,
-# 	'has_tv' : True,
-# 	'accepts_credit_card': True,
-# 	'drive_thru': True,
-# 	'waiter_service' : True,
-# 	'host_large_group' : True,
-# }
+if __name__ == '__main__':
+		app.run(debug=True)
