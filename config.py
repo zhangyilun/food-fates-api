0cd60a21eb6527afa2f7c41741b51d29496e98b8
Joy Gao <joyg@wepay.com>
2015-08-22 03:28:29 -0700

    a

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ TOKEN = 'BAWA8bXq7Aix-JwxJEBXXuT3KbH4g32I'
 TOKEN_SECRET = '5RrUCbSQ52uihSs0-tF8Aq0zDxE'
 
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://changeme:changeme@sixsense.c5v9p4w2iexs.us-east-1.rds.amazonaws.com:3306/"
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://changeme:changeme@sixsense.c5v9p4w2iexs.us-east-1.rds.amazonaws.com:3306/"
 
